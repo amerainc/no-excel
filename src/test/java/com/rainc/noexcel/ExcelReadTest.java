4f41671dc2f4832c90b36f26b16a6f665ed3807a
zhengyuchen.zyc <zhengyuchen.zyc@gongdao.com>
2021-12-24 10:04:13 +0800

    增加错误行读取示例

--- src/test/java/com/rainc/noexcel/ExcelReadTest.java
+++ src/test/java/com/rainc/noexcel/ExcelReadTest.java
@@ -1,13 +1,19 @@
 package com.rainc.noexcel;
 
 import com.rainc.noexcel.builder.ExcelReaderBuilder;
+import com.rainc.noexcel.builder.ExcelWriterBuilder;
+import com.rainc.noexcel.meta.BaseErrMsg;
 import com.rainc.noexcel.read.ExcelReader;
+import com.rainc.noexcel.write.ExcelWriter;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 /**
  * excel读取测试
@@ -47,6 +53,27 @@ public class ExcelReadTest {
             excelReader.readDataConcurrent(testEntity -> {
                 System.out.println(testEntity);
             },executorService,5);
+        }
+    }
+
+    @Test
+    public void readerrorFile() {
+        //读取excel
+        String path = getClass().getResource("/").getPath()+"readFile.xls";
+        ExcelReaderBuilder<TestEntity> builder = ExcelReaderBuilder.builder(TestEntity.class);
+        List<TestEntity> testEntities;
+        try (ExcelReader<TestEntity> excelReader = builder.build(new File(path))) {
+            testEntities = excelReader.readData();
+        }
+        //过滤成功的数据
+        List<TestEntity> success = testEntities.stream().filter(BaseErrMsg::hasNotErrMsg).collect(Collectors.toList());
+        //过滤成功的数据
+        List<TestEntity> error = testEntities.stream().filter(BaseErrMsg::hasErrMsg).collect(Collectors.toList());
+        //将失败的数据重新导成excel
+        try (ExcelWriter<TestEntity> excelWriter = ExcelWriterBuilder.builder(TestEntity.class).build()) {
+            excelWriter.writeDataAndClose(error,new FileOutputStream("errFile.xls"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
